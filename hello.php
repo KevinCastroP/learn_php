cd0900b5cc0432362c01ea684fd3f9ef68c7fc68
KevinCastroP <castrok.c11@hotmail.com>
2020-05-20 23:55:42 -0500

    first step

---
+++ hello.php
@@ -0,0 +1,15 @@
+<?php
+    echo 'Hello PHP';
+    //asign variables
+    $var1 = 1;
+    $name = "KC";
+    $lastName = 'caspin';
+    //reasign variable
+    $name = 3030;
+    //verify variable type and amount of characters
+    var_dump($name);
+    //concatenate two variables option 1
+    $name = 'Kevin ' . $lastName;
+    //concatenate two variables option 2
+    $name = "Kevin $lastName";
+?>
